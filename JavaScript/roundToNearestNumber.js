e53b3451f8853c6f0c5068c433419b7b07de4276
larsliin <larsliin@gmail.com>
2013-01-15 15:36:26 +0100

    Update JavaScript/roundToNearestNumber.js
    providing example

--- JavaScript/roundToNearestNumber.js
+++ JavaScript/roundToNearestNumber.js
@@ -1,4 +1,6 @@
 // finds closests value in array
+// ex.: 
+// roundTo((100 / 4), 30); // returns 25
 function roundTo(increment, value){
     
     return Math.round(value / increment) * increment;
