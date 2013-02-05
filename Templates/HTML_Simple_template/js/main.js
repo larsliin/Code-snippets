ec95aa695b523082b8e6a3de252dbd508249d562
larsliin <larsliin@gmail.com>
2013-02-05 17:10:02 +0100

    Update Templates/HTML_Simple_template/js/main.js

--- Templates/HTML_Simple_template/js/main.js
+++ Templates/HTML_Simple_template/js/main.js
@@ -1,33 +1,33 @@
 (function($) {
-  var MyPlugin = function(element, options) {
-    var elem = $(element);
-    var obj = this;
-    var settings = $.extend({param : 'defaultValue'}, options || {});
+    var MyPlugin = function(element, options) {
+        var elem = $(element);
+        var obj = this;
+        var settings = $.extend({param : 'defaultValue'}, options || {});
 
-    // Public method - can be called from client code
-    this.publicMethod = function() {
-      init();
+        // Public method - can be called from client code
+        this.publicMethod = function() {
+            init();
+        };
+
+        // Private method - can only be called from within this object
+        var init = function() {
+            elem.text('Script file init');
+        };
     };
 
-    // Private method - can only be called from within this object
-    var init = function() {
-      elem.text('Script file init');
+    $.fn.myplugin = function(options) {
+        return this.each(function() {
+        var element = $(this);
+
+        // Return early if this element already has a plugin instance
+        if (element.data('myplugin'))
+            return;
+
+        // pass options to plugin constructor
+        var myplugin = new MyPlugin(this, options);
+
+        // Store plugin object in this element's data
+        element.data('myplugin', myplugin);
+        });
     };
-  };
-
-  $.fn.myplugin = function(options) {
-    return this.each(function() {
-      var element = $(this);
-
-      // Return early if this element already has a plugin instance
-      if (element.data('myplugin'))
-        return;
-
-      // pass options to plugin constructor
-      var myplugin = new MyPlugin(this, options);
-
-      // Store plugin object in this element's data
-      element.data('myplugin', myplugin);
-    });
-  };
 })(jQuery); 
