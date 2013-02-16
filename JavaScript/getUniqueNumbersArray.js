75ba975a015a0fc2d816cb188088f0406b5e55a4
larsliin <larsliin@gmail.com>
2013-02-16 13:57:20 +0100

    Update JavaScript/getUniqueNumbersArray.js

--- JavaScript/getUniqueNumbersArray.js
+++ JavaScript/getUniqueNumbersArray.js
@@ -1,15 +1,18 @@
 function getRandomArray(length, maxlength){
-  var arr = []
+	var arr = []
 	while(arr.length < length){
-		var randomnumber=Math.ceil(Math.random()*maxlength)
-		var found=false;
-		for(var i=0;i<arr.length;i++){
-			if(arr[i]==randomnumber){
-				found=true;
+		var randomnumber = Math.ceil(Math.random() * maxlength)
+		var found = false;
+
+		for(var i = 0;i < arr.length; i++){
+			if(arr[i] == randomnumber){
+				found = true;
 				break
 			}
 		}
-		if(!found)arr[arr.length]=randomnumber;
+		if(!found){
+			arr[arr.length] = randomnumber;
+		}
 	}
 	return arr;
 }
