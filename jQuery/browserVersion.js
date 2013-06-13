3bb26d92c645ee7655bce1fe86922cd8fe25aa2f
larsliin <larsliin@gmail.com>
2013-06-13 16:26:41 +0300

    Update browserVersion.js

--- jQuery/browserVersion.js
+++ jQuery/browserVersion.js
@@ -1,8 +1,8 @@
 // is browser IE8
-var isIE8 ($.browser.msie  && parseInt($.browser.version, 10) === 8) ? true : false;
+var isIE8 = ($.browser.msie  && parseInt($.browser.version, 10) === 8) ? true : false;
 
 // is browser IE9 or higher
-var isIE9OrHigher ($.browser.msie  && parseInt($.browser.version, 10) >= 9) ? true : false;
+var isIE9OrHigher = ($.browser.msie  && parseInt($.browser.version, 10) >= 9) ? true : false;
 
 // is browser IE
-var isIE ($.browser.msie) ? true : false;
+var isIE = ($.browser.msie) ? true : false;
