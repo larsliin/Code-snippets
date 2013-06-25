cbf3b5de6c9c82ada20c296c12a00c493b89e69d
larsliin <larsliin@gmail.com>
2013-06-25 11:29:27 +0200

    Create trimAndCompare.js

---
+++ JavaScript/trimAndCompare.js
@@ -0,0 +1,3 @@
+// Remove all whitespace, to lowercase and ready to compare
+var compare1 = (('I am compa   ring t e xt strings').toLowerCase()).replace(/ /g, '');
+var compare2 = (('i am compaRinG text STRINGS').toLowerCase()).replace(/ /g, '');
