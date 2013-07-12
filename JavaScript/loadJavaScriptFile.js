42d4a8bf622311a3a65437ffa008375504a7fe19
larsliin <larsliin@gmail.com>
2013-07-12 11:33:05 +0200

    Update loadJavaScriptFile.js

--- JavaScript/loadJavaScriptFile.js
+++ JavaScript/loadJavaScriptFile.js
@@ -1,17 +1,34 @@
 // dynamically loads a javascript file into header tag and throws an event on success
-function loadJS(src, callback) {
-    var s = document.createElement('script');
-    s.src = src;
-    s.async = true;
-    s.onreadystatechange = s.onload = function () {
-        var state = s.readyState;
+function loadJsCssFile(filename, filetype, callback) {
+    if (filetype == "js") { //if filename is a external JavaScript file
+        var fileref = document.createElement('script')
+        fileref.setAttribute("type", "text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype == "css") { //if filename is an external CSS file
+        var fileref = document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref != "undefined") {
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    }
+
+    fileref.onreadystatechange = fileref.onload = function () {
+        var state = fileref.readyState;
         if (!callback.done && (!state || /loaded|complete/.test(state))) {
             callback.done = true;
             callback();
         }
     };
-    document.getElementsByTagName('head')[0].appendChild(s);
 }
-loadJS('/Content/BeoVision12Campaign/JS/BeoVision12Campaign.js', function () {
-    // on success handler
+
+loadJsCssFile('/BeoVision12Campaign.js', 'js', function () {
+    // callback
+    console.log('Javascript loaded');
+});
+loadJsCssFile('/BeoVision12Campaign.css', 'css', function () {
+    // callback
+    console.log('Stylesheet loaded');
 });
