ee009eace6f26e3e7800bd676cfd8b4596f90b62
larsliin <larsliin@gmail.com>
2013-10-22 11:19:30 +0200

    Create jQueryLoadImages.js

---
+++ jQuery/jQueryLoadImages.js
@@ -0,0 +1,27 @@
+var $images = $('img');
+var imgTotal = $img.length;
+var loadImageCount = 0;
+
+function loadImage(j){
+    var $img = $images.eq(j)
+    var src = $img.attr('src');
+    
+    $img.one('load', function () {
+        loadImageComplete();
+    }).each(function () {
+        if (this.complete) $(this).load();
+    });
+    // ie needs the sourse reapplied
+    $img.attr('src', src);
+}
+
+function loadImageComplete(){
+    loadImageCount++;
+    if (loadImageCount < imgTotal) {
+        loadImage(loadImageCount);
+    } else {
+        // all images loaded
+    }
+}
+
+loadImage(loadImageCount);
