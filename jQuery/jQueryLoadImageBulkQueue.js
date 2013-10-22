b45b9f2e1d3f4d716f0317a4f692f754d819ea42
larsliin <larsliin@gmail.com>
2013-10-22 13:45:55 +0200

    Update jQueryLoadImageBulkQueue.js

--- jQuery/jQueryLoadImageBulkQueue.js
+++ jQuery/jQueryLoadImageBulkQueue.js
@@ -1,19 +1,33 @@
-var loaders = [];
-for (var i = 0; i < framesTotal; i++) {
-    loaders.push(loadSprite('imageUrl-' + i + '.jpg'));
+var $this = $(this);
+var $images = $this.find($('img'));
+
+$this.bind('images_loaded', allImagesLoadedHandler);
+
+loadImages($images);
+
+function loadImages($i) {
+    var loaders = [];
+    for (var i = 0; i < $i.length; i++) {
+        var url = $i.eq(i).attr('src');
+        console.log(url);
+        loaders.push(loadSprite(url));
+    }
+
+    $.when.apply(null, loaders).done(function () {            
+        $this.trigger('images_loaded');
+    });
+
+    function loadSprite(src) {
+        var deferred = $.Deferred();
+        var sprite = new Image();
+        sprite.onload = function () {
+            deferred.resolve();
+        };
+        sprite.src = src;
+        return deferred.promise();
+    }
 }
-
-$.when.apply(null, loaders).done(function () {
-    // callback when everything was loaded
-    alert('ALL LOADED');
-});
-
-function loadSprite(src) {
-    var deferred = $.Deferred();
-    var sprite = new Image();
-    sprite.onload = function () {
-        deferred.resolve();
-    };
-    sprite.src = src;
-    return deferred.promise();
+    
+function allImagesLoadedHandler(e) {
+    // all images loaded
 }
