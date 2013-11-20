4dc7d6a78ecb0e1c5488b20f850c8e9255b0a99d
larsliin <larsliin@gmail.com>
2013-11-20 10:47:10 +0100

    Delete hasAttr.js

--- jQuery/hasAttr.js
+++
@@ -1,6 +0,0 @@
-// returns bool if element has attribute
-function hasAttr($elem, attr){
-    return typeof $elem.attr(attr) !== 'undefined' && $elem.attr(attr) !== false ? true : false;
-}
-
-console.log(hasAttr($myelement, 'id'));
