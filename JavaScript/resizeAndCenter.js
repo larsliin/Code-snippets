6ed038706da5ef6d8726a1f7ea6c5cae972ba39e
larsliin <larsliin@gmail.com>
2014-03-19 11:34:29 +0100

    Create resizeAndCenter.js

---
+++ JavaScript/resizeAndCenter.js
@@ -0,0 +1,19 @@
+// resize and center element according to window dimensions
+// overflow is hidden
+function scale(obj) {
+	var widthRatio = $(window).width() / obj.orgwidth;
+	var heightRatio = $(window).height() / obj.orgheight;
+	var ratio = widthRatio;
+	var top;
+	var left;
+
+	if (widthRatio * obj.orgwidth < $(window).height()) {
+		ratio = heightRatio;
+		top = 0;
+		left = -1 * (((obj.orgwidth * ratio) - $(window).width()) / 2);
+	} else {
+		top = -1 * (((obj.orgheight * ratio) - $(window).height()) / 2);
+		left = 0;
+	}
+	obj.elem.css({ 'top': top + 'px', 'left': left + 'px' }).width(obj.orgwidth * ratio).height(obj.orgheight * ratio);
+}
