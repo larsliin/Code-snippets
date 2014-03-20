48ed0b1406a817f9adb12db98db5a5b0aa83a8e9
larsliin <larsliin@gmail.com>
2014-03-20 11:18:44 +0100

    Update browserVersion.js

--- JavaScript/browserVersion.js
+++ JavaScript/browserVersion.js
@@ -6,6 +6,8 @@ var isIE9OrHigher = ($.browser.msie  && parseInt($.browser.version, 10) >= 9) ? 
 
 // is browser IE
 var isIE = ($.browser.msie) ? true : false;
+// is browser IE (IE11 support)
+var isIE = !!navigator.userAgent.match(/Trident.*rv[ :]*11\./) || $.browser.msie;
 
 // is browser mobile
 var isDeviceMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) ? true : false;
