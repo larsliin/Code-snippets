803d6d0e8d0994beb910ee27fa8f160842816193
larsliin <larsliin@gmail.com>
2014-03-21 14:00:12 +0100

    Update resizeAndCenter.js

--- JavaScript/resizeAndCenter.js
+++ JavaScript/resizeAndCenter.js
@@ -2,19 +2,15 @@
 // overflow is hidden
 // http://stackoverflow.com/questions/8158012/how-to-scale-an-image-to-full-screen-using-javascript
 function scale(obj) {
-	var widthRatio = $(window).width() / obj.orgwidth;
-	var heightRatio = $(window).height() / obj.orgheight;
-	var ratio = widthRatio;
-	var top;
-	var left;
+    var widthRatio = $(window).width() / obj.width;
+    var heightRatio = $(window).height() / obj.height;
+    var ratio = widthRatio; 
 
-	if (widthRatio * obj.orgwidth < $(window).height()) {
-		ratio = heightRatio;
-		top = 0;
-		left = -1 * (((obj.orgwidth * ratio) - $(window).width()) / 2);
-	} else {
-		top = -1 * (((obj.orgheight * ratio) - $(window).height()) / 2);
-		left = 0;
-	}
-	obj.elem.css({ 'top': top + 'px', 'left': left + 'px' }).width(obj.orgwidth * ratio).height(obj.orgheight * ratio);
+    if (widthRatio * obj.height < $(window).height()) {
+        ratio = heightRatio;
+    }
+
+    obj.image.width(obj.width * ratio)
+        .height(obj.height * ratio)
+        .css({ margin: 'auto',position: 'absolute',top: 0,bottom: 0,left: 0,right: 0});
 }
