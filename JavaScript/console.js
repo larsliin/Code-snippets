728259ce8eefe0f7c1e8ecb9a73dd0ba581b8a09
larsliin <larsliin@gmail.com>
2014-08-14 21:09:19 +0200

    Create console.js

---
+++ JavaScript/console.js
@@ -0,0 +1,25 @@
+$(document).on('ready', function(){
+	var arr = ['dog','cat','bird','fish','giraf','lion','fox','duck','cow'];
+
+	//debugger;
+
+	//console.group('test');
+
+	console.timeline('timeline_label'); // label is optional
+
+	console.time('Array initialize');
+
+	console.groupCollapsed('loggroup');
+
+	for (var i = arr.length - 1; i >= 0; i--) {
+		console.log(arr[i]);
+	};
+
+	console.groupEnd();
+
+	console.timelineEnd('timeline_label');
+
+	setTimeout(function(){
+		console.timeEnd("Array initialize");
+	}, 1000);
+});
