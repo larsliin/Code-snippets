4cbb0edb98540ccf6a095201bbda6b8c485177a6
larsliin <larsliin@gmail.com>
2014-11-26 16:24:01 +0100

    Update sortedMultidimensionalArray.js

--- JavaScript/sortedMultidimensionalArray.js
+++ JavaScript/sortedMultidimensionalArray.js
@@ -1,5 +1,5 @@
 // returns sorted multidimensional array
-
+// sort numeric
 var multidimensionalArray = new Array();
 multidimensionalArray = [{country : 'Germany', city : 'Berlin'},{country : 'Argentina', city : 'Buenos Aires'}];
 
@@ -10,3 +10,17 @@ function getSortedMultidimensionalArray(a, b) {
 }
 
 console.log(multidimensionalArray.sort(getSortedMultidimensionalArray));
+
+// sort alphabetically
+function getSortedData(arr, sortby) {
+    return arr.sort(function (a, b) {
+        var sorta = a[sortby].toLowerCase(), sortb = b[sortby].toLowerCase();
+        if (sorta < sortb)
+            return -1;
+        if (sorta > sortb)
+            return 1;
+        return 0;
+    });    
+}
+var names = new Array({ name: 'BOB', sort: 'z' }, { name: 'TOM', sort: 'a' }, { name: 'ROB', sort: 'f' }, { name: 'JON', sort: 'c' });
+console.log(getSortedData(names, 'sort'));
