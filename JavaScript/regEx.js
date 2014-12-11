8bf67240b90cdbd5dfb98a72e285f28ba3b51953
larsliin <larsliin@gmail.com>
2014-12-11 11:38:16 +0100

    Update regEx.js

--- JavaScript/regEx.js
+++ JavaScript/regEx.js
@@ -4,6 +4,9 @@ function isValidEmail(email) {
 	return filter.test(email);
 }
 
+// or
+var isValidEmail = (function () { var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; return filter.test('my@email.com'); })();
+
 // is danish phonenumber
 function isValidPhonenumber(phonenumber) {
 	var filter = /((\(?\+45\)?)?)(\s?\d{2}\s?\d{2}\s?\d{2}\s?\d{2})/;
